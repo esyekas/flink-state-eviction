a9b9fa510cf5a9816591dbe5f916ed669703d94d
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2016-11-08 21:26:26 -0800

    complete poc of parallel all window

--- src/main/java/com/github/juanrh/streaming/windowAllPoCs/WindowAllTimeKeyedPoC.java
+++ src/main/java/com/github/juanrh/streaming/windowAllPoCs/WindowAllTimeKeyedPoC.java
@@ -6,24 +6,18 @@ import org.apache.flink.api.common.time.Time;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.AllWindowedStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
-import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.WindowAssigner;
-import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.api.windowing.windows.Window;
-import org.apache.flink.util.Collector;
-
-/*
-* FIXME: redo for reduce function that is the only that makes sense
-* */
 
 /**
  * Created by juanrh on 11/6/2016.
@@ -54,12 +48,15 @@ public class WindowAllTimeKeyedPoC {
      * This only make sense for window assigners that ensure the subwindows will be
      * in sync, like time based window assigners. This doesn't work for counting
      * or sessions window assigners
+     *
+     * Also note elements from different partitions might get out of order due
+     * to parallelism
      * */
     public static class ParAllWindowedStream<T,W extends Window> extends WindowedStream<T, Integer, W> {
-        private final transient WindowAssigner<? super T,W> windowAssigner;
+        private final transient WindowAssigner<Object,W> windowAssigner;
 
         public ParAllWindowedStream(DataStream<T> stream, final int parallelism,
-                                    WindowAssigner<? super T,W> windowAssigner) {
+                                    WindowAssigner<Object,W> windowAssigner) {
             super(stream.keyBy(new KeySelector<T, Integer>() {
                                @Override
                                 public Integer getKey(T value) throws Exception {
@@ -76,10 +73,22 @@ public class WindowAllTimeKeyedPoC {
                     .windowAll(windowAssigner)  // synchronize
                     .reduce(reduceFun);         // sequential aggregate of
         }
+
+        // Cannot override because we need an additional reduce function of type R
+        // to recombine the result for each window
+        // @Override
+        public <R> SingleOutputStreamOperator<R> applyPar(ReduceFunction<T> reduceFunction,
+                                                        WindowFunction<T, R, Integer, W> function,
+                                                        ReduceFunction<R> reduceWindowsFunction) {
+            return super.apply(reduceFunction, function)
+                        .windowAll(windowAssigner)
+                         .reduce(reduceWindowsFunction);
+        }
     }
 
-
     /*
+    *
+Execution with processing time
     *
 1> allWindowedReduced: (a-b,3)
 2> allWindowedReduced: (a-c,7)
@@ -87,23 +96,52 @@ public class WindowAllTimeKeyedPoC {
 3> allWindowedReduced: (b-c,12)
 2> parAllWindowedApplied: (a-c,7)
 4> allWindowedReduced: (a-c,17)     <-- missing last element for par if the last gap is too tight: in this example last gap of 500
-3> parAllWindowedApplied: (c-b,12)  <-- out of order for par
+3> parAllWindowedApplied: (c-b,12)  <-- out of order b-c due to parallelism
+
+Other execution with processing time: mixed elements in different windows
+*
+* 1> allWindowedReduced: (a-b,3)
+2> allWindowedReduced: (a-c,7)
+1> parAllWindowedApplied: (a-b-a,6)
+3> allWindowedReduced: (b-c,12)
+2> parAllWindowedApplied: (c,4)
+4> allWindowedReduced: (a-c,17)
+3> parAllWindowedApplied: (c-b,12)
+
+So we get non-determinism with processing time, as expected
+
+With ingestion time we get robust determinism in the windows, although we get
+reordered elements in the same window due to parallelism
+
+1> allWindowedReduced: (a-b,3)
+1> parAllWindowedApplied: (a-b,3)
+2> allWindowedReduced: (a-c,7)
+3> allWindowedReduced: (b-c,12)
+2> parAllWindowedApplied: (a-c,7)
+3> parAllWindowedApplied: (b-c,12)
+4> allWindowedReduced: (a-c,17)
+4> parAllWindowedApplied: (a-c,17)
     * */
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStateBackend(new MemoryStateBackend());
         env.getConfig().disableSysoutLogging();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime); // activate ingestion time
 
         SourceFunction<Tuple2<String, Integer>> source =
                 ElementsWithGapsSource
-                        .addElem(Tuple2.of("a", 1)).addGap(Time.milliseconds(20)).addElem(Tuple2.of("b", 2)).addGap(Time.milliseconds(500))
+                        // NOTE a gap of even 10 ms inside the window lead to occasional non determinism with the
+                        // window split in a small number of executions
+                        .addElem(Tuple2.of("a", 1)).addElem(Tuple2.of("b", 2)).addGap(Time.milliseconds(500))
                         .addElem(Tuple2.of("a", 3)).addElem(Tuple2.of("c", 4)).addGap(Time.milliseconds(500))
                         .addElem(Tuple2.of("b", 5)).addElem(Tuple2.of("c", 7)).addGap(Time.milliseconds(500))
-                        .addElem(Tuple2.of("a", 8)).addElem(Tuple2.of("c", 9)).addGap(Time.milliseconds(800)).build();
+                        .addElem(Tuple2.of("a", 8)).addElem(Tuple2.of("c", 9)).addGap(Time.milliseconds(500)).build();
         DataStream<Tuple2<String, Integer>> input = env.addSource(source);
 
         org.apache.flink.streaming.api.windowing.time.Time windowSize = org.apache.flink.streaming.api.windowing.time.Time.milliseconds(500);
-        WindowAssigner<Object, TimeWindow> windowAssigner = TumblingProcessingTimeWindows.of(windowSize);
+        WindowAssigner<Object, TimeWindow> windowAssigner =
+                TumblingEventTimeWindows.of(windowSize); // this is ingestion time due to the time characteristic
+                //TumblingProcessingTimeWindows.of(windowSize);
 
         final ReduceFunction<Tuple2<String, Integer>> redFun = new ReduceFunction<Tuple2<String, Integer>>() {
             @Override
@@ -126,10 +164,7 @@ public class WindowAllTimeKeyedPoC {
         DataStream<Tuple2<String, Integer>> parAllWindowedApplied = parAllWindowed.reduce(redFun);
         WindowAllKeyedPoC.printWithName(parAllWindowedApplied, "parAllWindowedApplied");
 
-                // TODO print keyed version to see the 1 window per paralelism
-
-        // TODO print parallel version of allWindow that should print just 1 window in total
-
         env.execute();
     }
+
 }
