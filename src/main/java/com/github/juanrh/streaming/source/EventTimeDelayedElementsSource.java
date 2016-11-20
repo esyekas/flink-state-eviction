935a65cb7706dc3744e7cfe002a4beeb9fe723d3
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2016-11-20 13:07:00 -0800

    add simple test for EventTimeDelayedElementsSource

--- src/main/java/com/github/juanrh/streaming/source/EventTimeDelayedElementsSource.java
+++ src/main/java/com/github/juanrh/streaming/source/EventTimeDelayedElementsSource.java
@@ -1,17 +1,17 @@
 package com.github.juanrh.streaming.source;
 
-import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Supplier;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.apache.flink.api.common.functions.MapFunction;
 
-import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
@@ -21,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -32,13 +31,21 @@ import java.util.List;
  * generated, that is used to assign that as the event time of each element. Hence the DataStream
  * returned by get() should be used with env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
  *
- * That time is interpreted as a relative time, i.e. a time delta from an initial time
- * that is the the current time at the call of get() as returned by System.currentTimeMillis(). The
- * absolute time build as initial time + delta is assigned as the event time of the element
+ * That delay is interpreted as a relative time, i.e. a time delta from an arbitrary initial time
+ *
+ * Currently there are two implementations
+ *  - earlyImplementation = true: the initial time is is the the current time at the call of get() as returned by
+ *  System.currentTimeMillis(). The absolute time build as initial time + delta is assigned as the event time of
+ *  the element. This means we get early elements, i.e. elements with a timestamp in the future wrt to processing
+ *  time. AscendingTimestampExtractor is used in this implementation, after sorting the elements by time
+ *
+ * - earlyImplementation = false: this is the default. The initial time is set to the current time minus the
+ * sum of all delays, so all events are late events, and a BoundedOutOfOrdernessTimestampExtractor is
+ * configured so no late event is dropped.
  *
  * NOTE this source doesn't delay the emission of elements, and instead elements are emitted instantaneously.
- * As a result the event time for an element might be later than the event ingestion time: we get early
- * events, instead of late events!
+ * So the event timestamp will only be relevant when window operations are applied, that do use the event
+ * time stamp for the aggregation operation that is applied to the window
  * */
 public class EventTimeDelayedElementsSource<T> implements Supplier<DataStream<T>> {
     private static final transient Logger LOG = LoggerFactory.getLogger(EventTimeDelayedElementsSource.class);
@@ -62,15 +69,18 @@ public class EventTimeDelayedElementsSource<T> implements Supplier<DataStream<T>
     private final TypeInformation<T> elementsTypeInfo;
     private final StreamExecutionEnvironment env;
 
-    /**
-     * Time specified in elements are relative to a start time, that will be
-     * the current time at the call of get() as returned by System.currentTimeMillis()
-     * */
+    @Accessors(chain = true) @Setter
+    private boolean earlyImplementation = false;
+
     public EventTimeDelayedElementsSource(StreamExecutionEnvironment env,
                                           Optional<TypeInformation<T>> elementsTypeInfo,
                                           final Iterable<Elem<T>> elements) {
         Preconditions.checkArgument(elements.iterator().hasNext(),
                 "EventTimeDelayedElementsSource needs at least one element");
+        Preconditions.checkArgument(env.getStreamTimeCharacteristic().equals(TimeCharacteristic.EventTime),
+                                    "EventTimeDelayedElementsSource only works with TimeCharacteristic.EventTime, %s found",
+                                    env.getStreamTimeCharacteristic());
+
         this.elementsTypeInfo = SourceUtils.getTypeInformation(elementsTypeInfo,
                 new Supplier<T>() {
                     @Override
@@ -80,6 +90,17 @@ public class EventTimeDelayedElementsSource<T> implements Supplier<DataStream<T>
                 });
         this.env = env;
         this.elements = Lists.newArrayList(elements);
+    }
+
+    public EventTimeDelayedElementsSource(StreamExecutionEnvironment env,
+                                          TypeInformation<T> elementsTypeInfo,
+                                          final Iterable<Elem<T>> elements) {
+        this(env, Optional.of(elementsTypeInfo), elements);
+    }
+
+    public EventTimeDelayedElementsSource(StreamExecutionEnvironment env,
+                                          final Iterable<Elem<T>> elements) {
+        this(env, Optional.<TypeInformation<T>>absent(), elements);
     }
 
     public static <T> EventTimeDelayedElementsSource<T> withEqualGaps(StreamExecutionEnvironment env,
@@ -92,6 +113,7 @@ public class EventTimeDelayedElementsSource<T> implements Supplier<DataStream<T>
      * @param gap relative gap between each element and the following
      * */
     public static <T> EventTimeDelayedElementsSource<T> withEqualGaps(StreamExecutionEnvironment env,
+                                                                      Optional<TypeInformation<T>> elementsTypeInfo,
                                                                       final Time gap,
                                                                       Iterable<T> elements) {
         Preconditions.checkArgument(elements.iterator().hasNext(),
@@ -103,19 +125,31 @@ public class EventTimeDelayedElementsSource<T> implements Supplier<DataStream<T>
             totalDelayMillis += gap.toMilliseconds();
         }
 
-        return new EventTimeDelayedElementsSource<>(
-                env,
-                Optional.<TypeInformation<T>>absent(), // FIXME
-                delayedElements);
+        return new EventTimeDelayedElementsSource<>(env, elementsTypeInfo, delayedElements);
     }
 
+    public static <T> EventTimeDelayedElementsSource<T> withEqualGaps(StreamExecutionEnvironment env,
+                                                                       TypeInformation<T> elementsTypeInfo,
+                                                                       final Time gap,
+                                                                       Iterable<T> elements) {
+        return withEqualGaps(env, Optional.of(elementsTypeInfo), gap, elements);
+    }
+
+    public static <T> EventTimeDelayedElementsSource<T> withEqualGaps(StreamExecutionEnvironment env,
+                                                                      final Time gap,
+                                                                      Iterable<T> elements) {
+        return withEqualGaps(env, Optional.<TypeInformation<T>>absent(), gap, elements);
+    }
     /**
      * The interval (every n milliseconds) in which the watermark will be generated
      * is defined via ExecutionConfig.setAutoWatermarkInterval
      * */
     @Override
     public DataStream<T> get() {
-        return getEarly();
+        if (earlyImplementation) {
+            return getEarly();
+        }
+        return getLate();
     }
 
     private DataStream<T> getLate() {
@@ -123,19 +157,20 @@ public class EventTimeDelayedElementsSource<T> implements Supplier<DataStream<T>
         for (Elem<T> elem : elements) {
             totalDelayMillis += elem.getDelay().toMilliseconds();
         }
-        LOG.warn("totalDelayMillis = {}", totalDelayMillis);
+        LOG.debug("totalDelayMillis = {}", totalDelayMillis);
 
         final long startTimestamp = System.currentTimeMillis() - totalDelayMillis;
         return env.fromCollection(elements)
-           .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<Elem<T>>(Time.milliseconds(totalDelayMillis*2)) {
-               @Override
-               public long extractTimestamp(Elem<T> elem) {
-                   long timestamp = startTimestamp + elem.getDelay().toMilliseconds();
-                   LOG.warn("Assigning timestamp of {}  to element {}",
-                             timestamp,  elem.getValue());
-                   return timestamp;
-               }
-           })
+           .assignTimestampsAndWatermarks(
+                   new BoundedOutOfOrdernessTimestampExtractor<Elem<T>>(Time.milliseconds(totalDelayMillis*2)) {
+                       @Override
+                       public long extractTimestamp(Elem<T> elem) {
+                           long timestamp = startTimestamp + elem.getDelay().toMilliseconds();
+                            LOG.debug("Assigning timestamp of {}  to element {}",
+                                      timestamp,  elem.getValue());
+                            return timestamp;
+                        }
+                   })
            .map(new MapFunction<Elem<T>, T>() {
                @Override
                public T map(Elem<T> elem) throws Exception {
@@ -155,8 +190,8 @@ public class EventTimeDelayedElementsSource<T> implements Supplier<DataStream<T>
                @Override
                public long extractAscendingTimestamp(Elem<T> elem) {
                         long timestamp = startTimestamp + elem.getDelay().toMilliseconds();
-                        LOG.warn("Assigning timestamp of {}  to element {}",
-                                timestamp,  elem.getValue());
+                        LOG.debug("Assigning timestamp of {}  to element {}",
+                                 timestamp,  elem.getValue());
                         return timestamp;
                     }
                 })
--- src/test/java/com/github/juanrh/streaming/source/EventTimeDelayedElementsSourceTest.java
+++ src/test/java/com/github/juanrh/streaming/source/EventTimeDelayedElementsSourceTest.java
@@ -1,8 +1,6 @@
 package com.github.juanrh.streaming.source;
 
-import com.google.common.collect.ContiguousSet;
-import com.google.common.collect.DiscreteDomain;
-import com.google.common.collect.Range;
+import com.google.common.collect.*;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -10,6 +8,11 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.util.StreamingMultipleProgramsTestBase;
+import org.apache.flink.contrib.streaming.DataStreamUtils;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Test;
 
 import java.util.List;
@@ -19,7 +22,6 @@ Following
     https://ci.apache.org/projects/flink/flink-docs-master/api/java/org/apache/flink/streaming/util/StreamingMultipleProgramsTestBase.html
 */
 public class EventTimeDelayedElementsSourceTest  extends StreamingMultipleProgramsTestBase {
-    final List<Integer> inputList = ContiguousSet.create(Range.closed(1, 6), DiscreteDomain.integers()).asList();
 
     public static StreamExecutionEnvironment getEnv() {
         // get shared execution context
@@ -31,24 +33,43 @@ public class EventTimeDelayedElementsSourceTest  extends StreamingMultipleProgra
         return env;
     }
 
-    /**
-     * This seems to work both with early and late events
-     * */
     @Test
-    public void fixme() throws Exception {
+    public void simpleWindow_EventTimeDelayedElementsSourceWithEqualGapsEarly_RespectsTime() throws Exception {
+        simpleWindow_EventTimeDelayedElementsSourceWithEqualGaps_RespectsTime(true);
+    }
+
+    @Test
+    public void simpleWindow_EventTimeDelayedElementsSourceWithEqualGapsLate_RespectsTime() throws Exception {
+        simpleWindow_EventTimeDelayedElementsSourceWithEqualGaps_RespectsTime(false);
+    }
+
+
+    public void simpleWindow_EventTimeDelayedElementsSourceWithEqualGaps_RespectsTime(boolean earlyImplementation) throws Exception {
         final StreamExecutionEnvironment env = getEnv();
 
+        List<Integer> inputList = ContiguousSet.create(Range.closed(1, 6), DiscreteDomain.integers()).asList();
         DataStream<Integer> input =
-                EventTimeDelayedElementsSource.withEqualGaps(env, Time.milliseconds(500), inputList).get();
-        input.timeWindowAll(Time.seconds(1))
+                EventTimeDelayedElementsSource.withEqualGaps(env, Time.milliseconds(500), inputList)
+                        .setEarlyImplementation(earlyImplementation)
+                        .get();
+
+        DataStream<Integer> result =  input.timeWindowAll(Time.seconds(1))
               .reduce(new ReduceFunction<Integer>() {
                   @Override
                   public Integer reduce(Integer i1, Integer i2) throws Exception {
                       return i1 + i2;
                   }
-              })
-            .print();
+              });
+        result.print();
 
-        env.execute();
+        List<Integer> collectedResult = ImmutableList.copyOf(DataStreamUtils.collect(result));
+        // env.execute(); // DataStreamUtils.collect already call env.execute(), calling it
+        // again here leads to exceptions
+
+        // Two outputs are possible depending on whether the first element gets it's own
+        // window or not, which is non deterministic
+        List<Integer> expectedResult1 = ImmutableList.of(3, 7, 11); // windows (1,2) (3,4), (5,6)
+        List<Integer> expectedResult2 = ImmutableList.of(1, 5, 9, 6); // windows (1) (2,3) (4, 5) (6)
+        assertThat(collectedResult, anyOf(is(expectedResult1), is(expectedResult2)));
     }
 }
