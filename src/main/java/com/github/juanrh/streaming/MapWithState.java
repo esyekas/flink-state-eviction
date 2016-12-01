fa309b22c3f5743fb0a03e4bb6285d75d0f796e2
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2016-11-30 23:08:28 -0800

    WIP generalizing PoC, still problems with type erasure

---
+++ src/main/java/com/github/juanrh/streaming/MapWithState.java
@@ -0,0 +1,303 @@
+package com.github.juanrh.streaming;
+
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Supplier;
+import com.google.common.collect.Lists;
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.experimental.Delegate;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.checkpoint.Checkpointed;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.IterativeStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.types.Either;
+import org.apache.flink.util.Collector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by juanrh on 11/25/2016.
+ *
+ * TODO:
+ *  - how to encapsulate: there is any equivalent to DataSet.runOperation for DataStream?
+ *  - how to get the KeySelector from a KeyedStream? ==> see how they do it in mapWithState
+ */
+/*
+* TODO extend like this was a method of KeyedDatastream
+*  - take DataStream<T> as self in constructor
+*  - have chained setter for other arguments: if default make sense; should feel like a map
+* in the interface in the degenerate case
+*  - ask for KeySelector for now
+* */
+//@RequiredArgsConstructor
+@Accessors(fluent = true)
+public class MapWithState<In, Key, State, Out> implements Supplier<SingleOutputStreamOperator<Out>>   {
+    private static final Logger LOG = LoggerFactory.getLogger(MapWithState.class);
+
+    private final transient DataStream<In> input;
+    private final MapWithState.Function<In, State, Out> mapFunction;
+    private final State defaultState;
+
+    @Setter @Getter
+    private transient Time ttl = null;
+    @Setter @Getter
+    private transient Time ttlRefreshInterval = null;
+    @Setter @Getter
+    private KeySelector<In, Key> keySelector = null;
+
+    // TODO memoize in lazy memoized supplier?
+    @Override
+    public SingleOutputStreamOperator<Out> get() {
+        Preconditions.checkState(ttl != null && ttl.toMilliseconds()> 0,
+                                 "ttlMillis should be non null and greater than 0, %s found", ttl.toMilliseconds());
+        // TODO other preconditions
+
+        DataStream<Either<In, Key>> eitherInputOrTombstone =
+                input.map(new MapFunction<In, Either<In, Key>>() {
+                    @Override
+                    public Either<In, Key> map(In in) throws Exception {
+                        return Either.Left(in);
+                    }
+                });
+        // FIXME make timeout configurable
+        IterativeStream<Either<In, Key>> eitherInputOrTombstoneIter =
+                eitherInputOrTombstone.iterate(Time.seconds(5).toMilliseconds());
+        DataStream<Either<Out, Key>> trans =
+                eitherInputOrTombstoneIter
+                        // required to avoid java.lang.RuntimeException: State key serializer has not been configured
+                        // in the config. This operation cannot use partitioned state.
+                        .keyBy(new KeySelector<Either<In,Key>, Key>() {
+                            @Override
+                            public Key getKey(Either<In, Key> either) throws Exception {
+                                if (either.isLeft()) {
+                                    return keySelector.getKey(either.left());
+                                }
+                                return either.right();
+                            }
+                        })
+                        .flatMap(this.new CoreTransformationFunction());
+
+        DataStream<Either<In, Key>> closing = trans.flatMap(new FlatMapFunction<Either<Out, Key>, Either<In, Key>>() {
+            @Override
+            public void flatMap(Either<Out, Key> either, Collector<Either<In, Key>> collector) throws Exception {
+                if (either.isRight()) {
+                    // collector.collect(either); // this fails
+                    // this makes the type transformation, and that's why we cannot use a filter
+                    collector.collect(Either.<In, Key>Right(either.right()));
+                }
+            }
+        });
+        eitherInputOrTombstoneIter.closeWith(closing);
+        SingleOutputStreamOperator<Out> output = trans.flatMap(new FlatMapFunction<Either<Out, Key>, Out>() {
+            @Override
+            public void flatMap(Either<Out, Key> either, Collector<Out> collector) throws Exception {
+                if (either.isLeft()) {
+                    collector.collect(either.left());
+                }
+            }
+        });
+        // this does nothing due to type erasure
+        //.returns(TypeInformation.of(new TypeHint<Out>() {}));
+        return output;
+    }
+
+    public MapWithState(DataStream<In> input,
+                        MapWithState.Function<In, State, Out> mapFunction,
+                        State defaultState){
+        this.input = input;
+        this.mapFunction = mapFunction;
+        this.defaultState = defaultState;
+    }
+
+    public interface Function<In, State, Out> extends org.apache.flink.api.common.functions.Function {
+        // FIXME add note about exceotion thrown just like https://ci.apache.org/projects/flink/flink-docs-master/api/java/org/apache/flink/api/common/functions/MapFunction.html
+        // and above all double check this behaves the same as that, i.e. "Throwing an exception will cause the operation to fail and may trigger recovery"
+        // when used below
+        Out map(In value, ValueState<State> state) throws Exception ; // HERE start writing a test to see if this interface works
+    }
+
+
+    // FIXME: make this truly non null by adding a Optional<T>, and remove the
+    // default value and use Optional.absent instead
+    /**
+     * This class is only a mutable container for a value of type T, with
+     * additional access info to implementing the time to live, and that
+     * implements ValueState to have an interface familiar to users of
+     * ValueState when implementing a MapWithStateFunction
+     *
+     * Note this object is in fact persisted in a ValueState<TimeStampedValue<T>> */
+    public static class TimeStampedValue<T> implements Serializable, ValueState<T> {
+        private static final long serialVersionUID = 1L;
+
+        @NonNull
+        private T value;
+        /** millis since UNIX epoch like in System.currentTimeMillis */
+        private long lastAccessTimestamp;
+        private boolean firstTombtoneSent;
+
+        public TimeStampedValue(@NonNull T value) {
+            this.value = value;
+            this.lastAccessTimestamp = 0;
+            this.firstTombtoneSent = false;
+        }
+
+        @Override
+        public T value() throws IOException {
+            // update state for the current key was last touched now
+            lastAccessTimestamp = System.currentTimeMillis();
+            return value;
+        }
+
+        @Override
+        public void update(T value) throws IOException {
+            this.value = value;
+        }
+
+        @Override
+        public void clear() {
+            this.value = null;
+        }
+    }
+
+    @RequiredArgsConstructor
+    public class CoreTransformationFunction extends RichFlatMapFunction<Either<In, Key>, Either<Out, Key>>
+                                            implements Serializable, Checkpointed<LinkedList<Key>> {
+        private static final long serialVersionUID = 1L;
+
+        private final long ttlMillis = MapWithState.this.ttl.toMilliseconds();
+        private final long ttlRefreshIntervalMillis = MapWithState.this.ttlRefreshInterval.toMilliseconds();
+
+        // FIXME consider replacing by Akka scheduler http://doc.akka.io/docs/akka/2.4.4/java/scheduler.html
+        // if the ActorContext is available somehow
+        private transient ValueState<TimeStampedValue<State>> valueState;
+        private transient ScheduledExecutorService executor;
+        private transient Set<Key> pendingTombstones;
+        private transient Optional<List<Key>> recoveringTombstones;
+
+        @Override
+        public void open(Configuration config) {
+            ValueStateDescriptor<TimeStampedValue<State>> descriptor =
+                new ValueStateDescriptor<>("sumWithTimestamp", // the state name
+                                           TypeInformation.of(new TypeHint<TimeStampedValue<State>>() {}),
+                                           new TimeStampedValue<>(defaultState));
+            valueState = getRuntimeContext().getState(descriptor);
+            executor = Executors.newSingleThreadScheduledExecutor();
+            pendingTombstones = Collections.newSetFromMap(new ConcurrentHashMap<Key, Boolean>());
+            recoveringTombstones = Optional.absent();
+        }
+
+        @Override
+        public void close() {
+            // just shutdown ASAP, it's ok to lose the pending
+            // tombstones as the state will be deleted anyway
+            executor.shutdownNow();
+            LOG.warn("Closed function");
+        }
+
+        private void sendTombstone(final Collector<Either<Out, Key>> collector, final Key key) {
+            LOG.warn("Scheduling the shipment of a tombstone for key {}", key);
+            pendingTombstones.add(key);
+            executor.schedule(new Runnable() {
+                @Override
+                public void run() {
+                    LOG.warn("Sending a tombstone for key {}", key);
+                    Either<Out, Key> tombstone = Either.Right(key);
+                    collector.collect(tombstone);
+                    pendingTombstones.remove(key);
+                }
+            }, ttlRefreshIntervalMillis, TimeUnit.MILLISECONDS);
+        }
+
+        // FIXME handle IOException accessing the state
+        @Override
+        public void flatMap(Either<In, Key> either, Collector<Either<Out, Key>> collector) throws Exception {
+            if (recoveringTombstones.isPresent()) {
+                for (Key tombstone : recoveringTombstones.get()) {
+                    LOG.warn("Recovering tombstone {}", tombstone);
+                    sendTombstone(collector, tombstone);
+                }
+                recoveringTombstones = Optional.absent();
+            }
+            final TimeStampedValue<State> state = valueState.value();
+            if (either.isLeft()) {
+                In inputValue = either.left();
+                // state can be mutated here by mapFunction, and access to
+                // the state with value() will update state.lastAccessTimestamp
+                // call this here before updating the state
+                Out outputValue = MapWithState.this.mapFunction.map(inputValue, state);
+                if (! state.firstTombtoneSent) {
+                    // sent tombstone to the same key: this is only required to send the first tombstone
+                    // FIXME: this only works if we have access to the key!!!
+                    state.firstTombtoneSent = true;
+                    sendTombstone(collector, MapWithState.this.keySelector.getKey(inputValue));
+                }
+                // store wrapping state in actual value state
+                valueState.update(state);
+                collector.collect(Either.<Out, Key>Left(outputValue));
+            } else {
+                // we just received a tombstone
+                LOG.warn("Received a tombstone for key {}", either.right());
+                long currentTimeMillis = System.currentTimeMillis();
+                if (currentTimeMillis - state.lastAccessTimestamp >= ttlMillis) {
+                    // evict and stop sending tombstones for this key
+                    // next time this key appears and it uses the state, the default state
+                    // that makes isTombstoneSent = false will be used, and that will send
+                    // the first tombstone of the new eviction cycle
+                    valueState.clear();
+                    LOG.warn("Evicted state for key " + either.right());
+                } else {
+                    // send another tombstone: the current tombstone prevented sending
+                    // more tombstone for events after the one that sent the current tombstone
+                    sendTombstone(collector, either.right());
+                }
+            }
+
+        }
+
+        // FIXME handle IOException accessing the state
+        @Override
+        public LinkedList<Key> snapshotState(long checkpointId, long checkpointTimestamp) throws Exception {
+            LinkedList<Key> state = Lists.newLinkedList(pendingTombstones);
+            if (recoveringTombstones.isPresent()){
+                state.addAll(recoveringTombstones.get());
+            }
+            return state;
+        }
+
+        @Override
+        public void restoreState(LinkedList<Key> tombstones) throws Exception {
+            /* Send all the pending tombstone during the recovery, which is a good enough
+            approximation even though would lead to sending the tombstones a little early */
+            // only have access to the collector in flatMap, keep in recoveringTombstones
+            // until next call to flatMap
+            recoveringTombstones = Optional.<List<Key>>of(Lists.newLinkedList(tombstones));
+        }
+    }
+}
---
+++ src/test/java/com/github/juanrh/streaming/MapWithStateTest.java
@@ -0,0 +1,76 @@
+package com.github.juanrh.streaming;
+
+import com.github.juanrh.streaming.source.ElementsWithGapsSource;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.util.StreamingMultipleProgramsTestBase;
+import org.junit.Test;
+
+
+/**
+ * Created by juanrh on 11/28/2016.
+ */
+public class MapWithStateTest extends StreamingMultipleProgramsTestBase {
+    public static StreamExecutionEnvironment getEnv() {
+        // get shared execution context
+        // https://ci.apache.org/projects/flink/flink-docs-master/api/java/org/apache/flink/streaming/util/StreamingMultipleProgramsTestBase.html
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStateBackend(new MemoryStateBackend());
+        env.getConfig().disableSysoutLogging();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime); // activate ingestion time
+        return env;
+    }
+
+    @Test
+    public void pairsValueState_MapWithState_StateIsEvictedCorrectly() throws Exception {
+        final StreamExecutionEnvironment env = getEnv();
+
+        SourceFunction<Tuple2<String, Integer>> source =
+                ElementsWithGapsSource
+                        .addElem(Tuple2.of("a", 2)).addGap(Time.milliseconds(500))
+                        .addElem(Tuple2.of("a", 3)).addGap(Time.milliseconds(500))
+                        .addElem(Tuple2.of("b", 1)).addGap(Time.milliseconds(500))
+                        .addElem(Tuple2.of("c", 5)).addElem(Tuple2.of("d", 2)).addGap(Time.seconds(1))
+                        .addElem(Tuple2.of("a", 3)).addElem(Tuple2.of("c", 2))
+                        .addGap(Time.seconds(2)).addElem(Tuple2.of("c", 1)).build();
+
+        DataStream<Tuple2<String, Integer>> input = env.addSource(source);
+
+        final MapWithState<Tuple2<String, Integer>, String, Integer, Tuple2<String, Integer>> mapWithState =
+            new MapWithState<Tuple2<String, Integer>, String, Integer, Tuple2<String, Integer>>(
+                input,
+                new MapWithState.Function<Tuple2<String, Integer>, Integer, Tuple2<String, Integer>>() {
+                    @Override
+                    public Tuple2<String, Integer> map(Tuple2<String, Integer> value, ValueState<Integer> state) throws Exception {
+                        final int currentSum = value.f1 + state.value();
+                        state.update(currentSum);
+                        return Tuple2.of(value.f0, currentSum);
+                    }
+                },
+                0) // default state
+                .ttl(Time.milliseconds(1100)) // how long a state key will last in processing time (approx)
+                .ttlRefreshInterval(Time.milliseconds(450)) // how often access to each state key will be checked (approx)
+                .keySelector(new KeySelector<Tuple2<String, Integer>, String>() {
+                    @Override
+                    public String getKey(Tuple2<String, Integer> stringIntegerTuple2) throws Exception {
+                        return stringIntegerTuple2.f0;
+                    }
+                });
+        DataStream<Tuple2<String, Integer>> mapped =
+                mapWithState.get()
+                .returns(TypeInformation.of(new TypeHint<Tuple2<String, Integer>>() {}));
+        // TODO collect and assert
+        // TODO ensure logging is customized to see all intermediate data as in the PoC
+        StreamingUtils.printWithName(mapped, "mapped");
+        env.execute(); // remove when using collector and assert
+    }
+}
