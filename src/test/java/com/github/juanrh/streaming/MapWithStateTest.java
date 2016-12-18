cb4f5efa350128a50f27299ee09d02fe6370c86a
Juan Rodriguez Hortala <juan.rodriguez.hortala@gmail.com>
2016-12-17 23:26:52 -0800

    add passing test for simple example

--- src/test/java/com/github/juanrh/streaming/MapWithStateTest.java
+++ src/test/java/com/github/juanrh/streaming/MapWithStateTest.java
@@ -1,11 +1,13 @@
 package com.github.juanrh.streaming;
 
 import com.github.juanrh.streaming.source.ElementsWithGapsSource;
+import com.google.common.collect.ImmutableList;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.contrib.streaming.DataStreamUtils;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -14,6 +16,12 @@ import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.util.StreamingMultipleProgramsTestBase;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 
 /**
@@ -40,7 +48,7 @@ public class MapWithStateTest extends StreamingMultipleProgramsTestBase {
                         .addElem(Tuple2.of("a", 3)).addGap(Time.milliseconds(500))
                         .addElem(Tuple2.of("b", 1)).addGap(Time.milliseconds(500))
                         .addElem(Tuple2.of("c", 5)).addElem(Tuple2.of("d", 2)).addGap(Time.seconds(1))
-                        .addElem(Tuple2.of("a", 3)).addElem(Tuple2.of("c", 2))
+                        .addElem(Tuple2.of("c", 2)).addGap(Time.seconds(1)).addElem(Tuple2.of("a", 3))
                         .addGap(Time.seconds(2)).addElem(Tuple2.of("c", 1)).build();
 
         DataStream<Tuple2<String, Integer>> input = env.addSource(source);
@@ -69,6 +77,29 @@ public class MapWithStateTest extends StreamingMultipleProgramsTestBase {
         // TODO collect and assert
         // TODO ensure logging is customized to see all intermediate data as in the PoC
         StreamingUtils.printWithName(mapped, "mapped");
-        env.execute(); // remove when using collector and assert
+
+        List<Tuple2<String, Integer>> collectedMapped = ImmutableList.copyOf(DataStreamUtils.collect(mapped));
+        //env.execute(); // DataStreamUtils.collect already call env.execute(), calling it
+                         // again here leads to exceptions
+
+        List<Tuple2<String, Integer>> expectedPrefix =
+                ImmutableList.of(Tuple2.of("a", 2), Tuple2.of("a", 5), Tuple2.of("b", 1));
+        List<Tuple2<String, Integer>> expectedSuffix =
+                ImmutableList.of(Tuple2.of("c", 7), Tuple2.of("a", 3), Tuple2.of("c", 1));
+
+
+        List<Tuple2<String, Integer>> expectedResult1 =
+                ImmutableList.<Tuple2<String, Integer>>builder()
+                        .addAll(expectedPrefix)
+                        .add(Tuple2.of("c", 5)).add(Tuple2.of("d", 2))
+                        .addAll(expectedSuffix)
+                        .build();
+        List<Tuple2<String, Integer>> expectedResult2 =
+                ImmutableList.<Tuple2<String, Integer>>builder()
+                        .addAll(expectedPrefix)
+                        .add(Tuple2.of("d", 2)).add(Tuple2.of("c", 5))
+                        .addAll(expectedSuffix)
+                        .build();
+        assertThat(collectedMapped, anyOf(is(expectedResult1), is(expectedResult2)));
     }
 }
